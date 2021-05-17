64a075bffb0c5c3c795cdb9214cb3e52ea4553de
Justin Bishop <jubishop@gmail.com>
2021-05-17 14:01:24 -0700

    skip context?

--- spec/google_spec.rb
+++ spec/google_spec.rb
@@ -3,8 +3,6 @@ require_relative '../lib/tony/google'
 FakeRequest = Struct.new(:base_url)
 
 RSpec.describe(Tony::Auth::Google, type: :feature) {
-  skip if Test::Env.github_actions?
-
   def login
     fill_in(type: 'email', with: 'jubitester@gmail.com')
     click_button('Next')
@@ -16,18 +14,22 @@ RSpec.describe(Tony::Auth::Google, type: :feature) {
     raise error
   end
 
-  it('passes email in LoginInfo') {
-    visit(Tony::Auth::Google.url(FakeRequest.new('http://localhost:31337')))
-    login
-    expect(page).to(have_content('jubitester@gmail.com'))
-  }
+  context('logging in') {
+    skip if Test::Env.github_actions?
 
-  it('passes through state in LoginInfo') {
-    visit(Tony::Auth::Google.url(FakeRequest.new('http://localhost:31337'),
-                                 redirect: '/'))
-    login
-    click_button('Allow')
-    File.write('google_login.txt', page.body)
-    expect(page).to(have_content(':redirect=>"/"'))
+    it('passes email in LoginInfo') {
+      visit(Tony::Auth::Google.url(FakeRequest.new('http://localhost:31337')))
+      login
+      expect(page).to(have_content('jubitester@gmail.com'))
+    }
+
+    it('passes through state in LoginInfo') {
+      visit(Tony::Auth::Google.url(FakeRequest.new('http://localhost:31337'),
+                                   redirect: '/'))
+      login
+      click_button('Allow')
+      File.write('google_login.txt', page.body)
+      expect(page).to(have_content(':redirect=>"/"'))
+    }
   }
 }
